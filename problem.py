74d51512f9d127eb60937ffb58d91d9cd99ccf08
shreya2057 <shreya2057@gmail.com>
2022-08-08 13:11:18 +0545

    problem solved

--- problem.py
+++ problem.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+import graphviz
 
 # Initial no. of missionaries, cannibals, and position of canoe/boat
 M, C, P = 3, 3, 0
@@ -13,8 +14,10 @@ class MissionariesCannibal:
     def __init__(self) -> None:
         __ini_state = (M, C, P)
         self.__graph_matrix: List[List[State]] = [[(__ini_state)]]
-        self.__possible_actions: List[Action] = [
-            (0, 1), (1, 0), (1, 1), (2, 0), (0, 2)]
+        self.__possible_actions: List[Action] = [(0, 1), (1, 0), (1, 1), (2, 0), (0, 2)]
+        self.f = graphviz.Digraph('finite_state_machine', filename='fsm.gv')
+        self.node_format = "[{}, {}, {}]"
+
 
     def isGoalState(self, state: State) -> bool:
         m, c, _ = state
@@ -31,13 +34,26 @@ class MissionariesCannibal:
             return False
         return (m, c, int(not bool(p)))
 
+    def addNode(self, current: State, child: State, action: Action, isGoal: bool) -> None:
+        pc, pm, pp = current
+        cc, cm, cp = child
+        ac, am = action
+        self.f.attr("node", shape="circle")
+        if isGoal:
+            self.f.attr("node", shape="doublecircle")
+        self.f.edge(self.node_format.format(pc, pm, pp),
+                    self.node_format.format(cc, cm, cp),
+                    label="({}, {})".format(ac, am))
+
     def generateNextStates(self, state: State) -> List[State]:
         states: List[State] = []
         for ac in self.__possible_actions:
             valid_state = self.isValidState(state, ac)
             if valid_state:
                 states.append(valid_state)
-                if self.isGoalState(valid_state):
+                goal = self.isGoalState(valid_state)
+                self.addNode(state, valid_state, ac, isGoal=goal)
+                if goal:
                     break
         return states
 
@@ -59,6 +75,7 @@ class MissionariesCannibal:
                         break
             row += 1
             self.__graph_matrix.append(states)
+        self.f.view()
         return self.__graph_matrix
 
 
